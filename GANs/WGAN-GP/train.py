5dd778a13fea92c13e6dcece19067546c3ab924c
sushant097 <sushant1234gautam@gmail.com>
2022-02-01 20:38:02 +0545

    Updated Training to support losses visualization

--- GANs/WGAN-GP/train.py
+++ GANs/WGAN-GP/train.py
@@ -3,6 +3,7 @@ from torchvision.utils import save_image
 from torchvision import datasets
 from torchvision.transforms import transforms
 import os
+import matplotlib.pyplot as plt
 
 from model import Generator, Discriminator, init_weights, denorm_image
 from config import *
@@ -25,7 +26,7 @@ transforms = transforms.Compose([
 
 dataloader = torch.utils.data.DataLoader(
     datasets.CIFAR10(
-        data_dir,
+        root=data_dir,
         download=True,
         train=True,
         transform=transforms
@@ -45,14 +46,14 @@ init_weights(critic)
 optimCritic = torch.optim.Adam(critic.parameters(), lr=learning_rate, betas=(0.0, 0.9))
 optimGen = torch.optim.Adam(gen.parameters(), lr=learning_rate, betas=(0.0, 0.9))
 
-
 # ------------
 #  Training
 # ------------
 
 # set the training mode
-gen.train()
-critic.train()
+# gen.train()
+# critic.train()
+gen_losses, critic_losses = [], []
 for epoch in range(epochs):
     for i, (imgs, _) in enumerate(dataloader):
 
@@ -64,8 +65,6 @@ for epoch in range(epochs):
         # --------------------
         for _ in range(critic_iter):
 
-            optimCritic.zero_grad()
-
             # sample noise as generator input
             z = torch.randn((batch_size, hidden_dim, 1, 1), dtype=torch.float, device=device)
 
@@ -75,9 +74,11 @@ for epoch in range(epochs):
             # compute gradient penalty
             gp = gradient_penalty(critic, real_imgs, fake_imgs, device=device)
             # Adversarial loss - Critic
-            critic_loss = (-torch.mean(critic(real_imgs)) + torch.mean(critic(fake_imgs))
-                           + lambda_GP * gp) # adding gradient penalty to loss
-
+            critic_loss = (
+                    -torch.mean(critic(real_imgs)) + torch.mean(critic(fake_imgs))
+                    + lambda_GP * gp
+            )  # adding gradient penalty to loss
+            optimCritic.zero_grad()
             critic_loss.backward(retain_graph=True)
             optimCritic.step()
 
@@ -87,19 +88,28 @@ for epoch in range(epochs):
 
         # Train the generator Min - E[critic(fake)] ~ Max E[critic(fake)]
 
-        optimGen.zero_grad() # zero the gradient
 
         # Adversarial Loss
         loss_G = -torch.mean(critic(fake_imgs))
 
+        optimGen.zero_grad()  # zero the gradient
         loss_G.backward()
         optimGen.step()
 
-        print('Epoch [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}'
-              .format(epoch, epochs, critic_loss.item(), loss_G.item()))
+        print('Epoch [{}/{}] Batch [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}'
+              .format(epoch+1, epochs, i+1, len(dataloader), critic_loss.item(), loss_G.item()))
+        gen_losses.append(loss_G.item())
+        critic_losses.append(critic_loss.item())
 
     save_image(denorm_image(fake_imgs), os.path.join(sample_dir, 'fake_images-{}.png'.format(epoch + 1)))
 
 # Save the model checkpoints
 torch.save(gen.state_dict(), 'G.ckpt')
 torch.save(critic.state_dict(), 'D.ckpt')
+
+plt.plot(gen_losses, '-')
+plt.plot(critic_losses, '-')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend(['Generator', 'Critic'])
+plt.title('Losses')
