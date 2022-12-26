b48ccbd4e246e322e6e4ed98c8a59a0caa14ea7e
sushant097 <sushant1234gautam@gmail.com>
2022-12-26 17:05:50 +0545

    Added training method for unet

---
+++ ObjectDetectionAndSegmentation/Unet/dataset.py
@@ -0,0 +1,35 @@
+from torch import nn
+from pathlib import Path
+
+import torch.utils.data
+from PIL import Image
+import numpy as np
+import os
+
+
+class CarvanaDataset(torch.utils.data.Dataset):
+    def __init__(self, images, image_dir, mask_dir, transform=None) -> None:
+        super().__init__()
+        
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = images
+        
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        mask_path = os.path.join(self.mask_dir, self.images[index].replace(".jpg", "_mask.gif"))
+        image = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32) 
+        mask[mask == 255.0] = 1.0
+        
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations['image']
+            mask = augmentations['mask']
+            
+        return image, mask
+    
